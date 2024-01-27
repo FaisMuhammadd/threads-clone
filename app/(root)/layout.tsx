60c45c67f92775e4bc362178a92db651872650af
pandawaa <pallakangoding@gmail.com>
2024-01-27 11:20:42 +0800

    Auth with Clerk

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,25 @@
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
+
+import "../globals.css"
+
+export const metadata = {
+  title: "Threads",
+  description: "A Next.js Meta Threads Applications",
+}
+
+const inter = Inter({ subsets: ["latin"] })
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter.className}`}>{children}</body>
+      </html>
+    </ClerkProvider>
+  )
+}
