43c4f7b1a8b17958f769fc7fde0d41a7417b9a3c
pandawaa <pallakangoding@gmail.com>
2024-01-27 13:45:01 +0800

    add component Topbar LeftSidebar RightSidebar Bottombar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,6 +2,10 @@ import { ClerkProvider } from "@clerk/nextjs"
 import { Inter } from "next/font/google"
 
 import "../globals.css"
+import Topbar from "@/components/shared/Topbar"
+import LeftSidebar from "@/components/shared/LeftSidebar"
+import RightSidebar from "@/components/shared/RightSidebar"
+import Bottombar from "@/components/shared/Bottombar"
 
 export const metadata = {
   title: "Threads",
@@ -18,7 +22,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className}`}>{children}</body>
+        <body className={`${inter.className}`}>
+          <Topbar />
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   )
