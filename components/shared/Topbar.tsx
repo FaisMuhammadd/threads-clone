912e2c146e7ccb635c18cf60a2436593bd37f41b
pandawaa <pallakangoding@gmail.com>
2024-01-27 15:29:01 +0800

    add basetheme Dark

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { dark } from "@clerk/themes"
 
 const Topbar = () => {
   const isUserLoggedIn = true
@@ -30,6 +31,7 @@ const Topbar = () => {
 
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
