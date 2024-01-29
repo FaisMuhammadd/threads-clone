30cfa1d2b602d74ee3d1b49bf23c0e6595bf93b3
pandawaa <pallakangoding@gmail.com>
2024-01-29 13:22:34 +0800

    feat: Activity

---
+++ app/(root)/activity/page.tsx
@@ -0,0 +1,52 @@
+import { fetchUser, getActivity } from "@/lib/actions/user.actions"
+import { currentUser } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+import { redirect } from "next/navigation"
+
+async function Page() {
+  const user = await currentUser()
+
+  if (!user) return null
+
+  const userInfo = await fetchUser(user.id)
+  if (!userInfo?.onboarded) redirect("/onboarding")
+
+  const activity = await getActivity(userInfo._id)
+
+  return (
+    <section>
+      <h1 className="head-text mb-10">Activity</h1>
+
+      <section className="mt-10 flex flex-col gap-5">
+        {activity.length > 0 ? (
+          <>
+            {activity.map((activiti) => (
+              <Link key={activiti._id} href={`/thread/${activiti.parentId}`}>
+                <article className="activity-card">
+                  <Image
+                    src={activiti.author.image}
+                    alt="Profile picture"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {activiti.author.name}
+                    </span>{" "}
+                    replied to your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="!text-base-regular text-light-3">No activity yet</p>
+        )}
+      </section>
+    </section>
+  )
+}
+
+export default Page
