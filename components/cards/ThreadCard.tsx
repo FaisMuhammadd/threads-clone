1189b41aa024badaa8b19550c566dc929cf7563b
pandawaa <pallakangoding@gmail.com>
2024-01-29 12:17:06 +0800

    modified Thread Card

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -66,7 +66,7 @@ const ThreadCard = ({
 
             <p className="mt-2 text-small-regular text-light-2">{content}</p>
 
-            <div className="mt-5 flex flex-col gap-3">
+            <div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}>
               <div className="flex gap-3.5">
                 <Image
                   src="/assets/heart-gray.svg"
