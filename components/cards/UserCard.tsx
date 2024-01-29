2b5a5657aed4886e9c102130aba9fdee942562f7
pandawaa <pallakangoding@gmail.com>
2024-01-29 13:03:05 +0800

    create User Card

---
+++ components/cards/UserCard.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import Image from "next/image"
+import { useRouter } from "next/navigation"
+import { Button } from "../ui/button"
+
+interface Props {
+  id: string
+  name: string
+  username: string
+  imgUrl: string
+  personType: string
+}
+
+const UserCard = ({ id, name, username, imgUrl, personType }: Props) => {
+  const router = useRouter()
+
+  return (
+    <article className="user-card">
+      <div className="user-card_avatar">
+        <Image
+          src={imgUrl}
+          alt="logo"
+          width={48}
+          height={48}
+          className="rounded-full"
+        />
+
+        <div className="flex-1 text-ellipsis">
+          <h4 className="text-base-semibold text-light-1">{name}</h4>
+          <p className="text-small text-gray-1">@{username}</p>
+        </div>
+      </div>
+
+      <Button
+        className="user-card_btn"
+        onClick={() => router.push(`/profile/${id}`)}
+      >
+        View
+      </Button>
+    </article>
+  )
+}
+
+export default UserCard
