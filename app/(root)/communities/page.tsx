1e92c0a64963368bdbfc591f5c082ea5d2b24d8a
pandawaa <pallakangoding@gmail.com>
2024-01-30 13:16:04 +0800

    modified communities page

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,8 +1,62 @@
-const Page = async () => {
+import CommunityCard from "@/components/cards/CommunityCard"
+import Pagination from "@/components/shared/Pagination"
+import Searchbar from "@/components/shared/Searchbar"
+import { fetchCommunities } from "@/lib/actions/community.actions"
+import { fetchUser } from "@/lib/actions/user.actions"
+import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined }
+}) => {
+  const user = await currentUser()
+  if (!user) return null
+
+  const userInfo = await fetchUser(user.id)
+  if (!userInfo?.onboarded) redirect("/onboarding")
+
+  const result = await fetchCommunities({
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  })
+
   return (
-    <section>
-      <h1 className="head-text mb-10">Communities</h1>
-    </section>
+    <>
+      <h1 className="head-text">Communities</h1>
+
+      <div className="mt-5">
+        <Searchbar routeType="communities" />
+      </div>
+
+      <section className="mt-9 flex flex-wrap gap-2">
+        {result.communities.length === 0 ? (
+          <p className="no-result">No Result</p>
+        ) : (
+          <>
+            {result.communities.map((community) => (
+              <CommunityCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                bio={community.bio}
+                members={community.members}
+              />
+            ))}
+          </>
+        )}
+      </section>
+
+      <Pagination
+        path="communities"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
+    </>
   )
 }
 
