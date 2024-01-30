f2431d5fde5299a3fb34363fd8ca39528a766320
pandawaa <pallakangoding@gmail.com>
2024-01-30 13:18:18 +0800

    modified profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -41,7 +41,7 @@ async function Page({ params }: { params: { id: string } }) {
                 <p className="max-sm:hidden">{tab.label}</p>
 
                 {tab.label === "Threads" && (
-                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                  <p className="ml-1 rounded-sm px-2 py-1  text-light-2">
                     {userInfo?.threads?.length}
                   </p>
                 )}
@@ -52,7 +52,7 @@ async function Page({ params }: { params: { id: string } }) {
             <TabsContent
               key={`content-${tab.label}`}
               value={tab.value}
-              className="w-full text-light-1"
+              className="w-full text-light-2"
             >
               <ThreadsTab
                 currentUserId={user.id}
