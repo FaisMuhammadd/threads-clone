2bcf73352763a51d83bdc88e6e4977656a8e26fc
pandawaa <pallakangoding@gmail.com>
2024-01-30 13:14:30 +0800

    modified Home Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,25 @@
 import ThreadCard from "@/components/cards/ThreadCard"
+import Pagination from "@/components/shared/Pagination"
 import { fetchPosts } from "@/lib/actions/thread.actions"
+import { fetchUser } from "@/lib/actions/user.actions"
 import { currentUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
 
-export default async function Home() {
-  const result = await fetchPosts(1, 30)
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined }
+}) {
   const user = await currentUser()
+  if (!user) return null
+
+  const userInfo = await fetchUser(user.id)
+  if (!userInfo?.onboarded) redirect("/onboarding")
+
+  const result = await fetchPosts(
+    searchParams.page ? +searchParams.page : 1,
+    10
+  )
 
   return (
     <>
@@ -31,6 +46,12 @@ export default async function Home() {
           </>
         )}
       </section>
+
+      <Pagination
+        path="/"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      />
     </>
   )
 }
