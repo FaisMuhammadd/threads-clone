0d76af1bdb251a7c8c280cc1ee56909859351b07
pandawaa <pallakangoding@gmail.com>
2024-01-30 13:23:53 +0800

    modified Topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -10,7 +10,7 @@ const Topbar = () => {
     <nav className="topbar">
       <Link href="/" className="flex items-center gap-4">
         <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
-        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+        <p className="text-heading3-bold text-light-4 max-xs:hidden">Threads</p>
       </Link>
 
       <div className="flex items-center gap-1">
