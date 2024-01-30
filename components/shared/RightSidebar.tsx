bdee7e3d32598182ef53f70ea4feffe5d3c3f91b
pandawaa <pallakangoding@gmail.com>
2024-01-30 13:22:39 +0800

    modified Right Sidebar

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,13 +1,67 @@
-const RightSidebar = () => {
+import { fetchCommunities } from "@/lib/actions/community.actions"
+import { fetchUsers } from "@/lib/actions/user.actions"
+import { currentUser } from "@clerk/nextjs"
+import UserCard from "../cards/UserCard"
+
+const RightSidebar = async () => {
+  const user = await currentUser()
+  if (!user) return null
+
+  const similarMinds = await fetchUsers({
+    userId: user.id,
+    pageSize: 4,
+  })
+
+  const suggestComunities = await fetchCommunities({ pageSize: 4 })
+
   return (
     <section className="custom-scrollbar rightsidebar">
       <div className="flex flex-1 flex-col justify-start">
         <h3 className="text-heading4-medium text-light-1">
           Suggested Communities
         </h3>
+
+        <div className="mt-7 flex w-[350px] flex-col gap-9">
+          {suggestComunities.communities.length > 0 ? (
+            <>
+              {suggestComunities.communities.map((community) => (
+                <UserCard
+                  key={community.id}
+                  id={community.id}
+                  name={community.name}
+                  username={community.username}
+                  imgUrl={community.image}
+                  personType="Community"
+                />
+              ))}
+            </>
+          ) : (
+            <p className="!text-base-regular text-light-3">
+              No communities yet
+            </p>
+          )}
+        </div>
       </div>
       <div className="flex flex-1 flex-col justify-start">
-        <h3 className="text-heading4-medium text-light-1">Suggest Users</h3>
+        <h3 className="text-heading4-medium text-light-1">Similar Minds</h3>
+        <div className="mt-7 flex w-[350px] flex-col gap-10">
+          {similarMinds.users.length > 0 ? (
+            <>
+              {similarMinds.users.map((person) => (
+                <UserCard
+                  key={person.id}
+                  id={person.id}
+                  name={person.name}
+                  username={person.username}
+                  imgUrl={person.image}
+                  personType="User"
+                />
+              ))}
+            </>
+          ) : (
+            <p className="!text-base-regular text-light-3">No users yet</p>
+          )}
+        </div>
       </div>
     </section>
   )
